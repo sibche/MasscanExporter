bbd057f3d157c861bbdd0ad760e2edc52daa0be1
ahmadali shafiee <mail@ahmadalli.net>
2020-08-01 16:33:07 +0430

    json propertyname fix
    Signed-off-by: ahmadali shafiee <mail@ahmadalli.net>

--- Models/MasscanOutput.cs
+++ Models/MasscanOutput.cs
@@ -8,12 +8,15 @@ namespace MasscanExporter.Models
 {
     public class MasscanOutput
     {
+        [JsonPropertyName("ip")]
         public string IP { get; set; }
+        [JsonPropertyName("ports")]
         public List<MasscanPortInfo> Ports { get; set; }
     }
 
     public class MasscanPortInfo
     {
+        [JsonPropertyName("port")]
         public int Port { get; set; }
         [JsonPropertyName("proto")]
         public string Protocol { get; set; }
