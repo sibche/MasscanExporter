0566a63d2a1b1d222869eb712629a5d4ef5eb09b
ahmadali shafiee <mail@ahmadalli.net>
2021-04-09 07:29:02 +0000

    find dotnet dynamically

--- deploy/systemd/install.sh
+++ deploy/systemd/install.sh
@@ -5,7 +5,26 @@ git clone https://github.com/sibche/MasscanExporter /tmp/MasscanExporter
 pushd /tmp/MasscanExporter
 systemctl stop masscan-exporter.service || true
 dotnet publish --configuration Release MasscanExporter.csproj -o /opt/masscan-exporter
-cp -f deploy/systemd/masscan-exporter.service /etc/systemd/system/
+cat << EOF > /etc/systemd/system/masscan-exporter.service
+[Unit]
+Description=masscan-exporter
+
+[Service]
+WorkingDirectory=/opt/masscan-exporter
+ExecStart=`which dotnet` /opt/masscan-exporter/MasscanExporter.dll
+Restart=always
+# Restart service after 10 seconds if the dotnet service crashes:
+RestartSec=10
+KillSignal=SIGINT
+SyslogIdentifier=masscan-exporter
+User=root
+Environment=ASPNETCORE_ENVIRONMENT=Production
+Environment=DOTNET_PRINT_TELEMETRY_MESSAGE=false
+
+[Install]
+WantedBy=multi-user.target
+
+EOF
 systemctl daemon-reload
 systemctl enable masscan-exporter.service
 systemctl restart masscan-exporter.service
